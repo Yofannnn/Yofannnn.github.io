3c5dd900e633aab7e637c915d58e5480e4e26f64
Yofannnn <yofanapril17@gmail.com>
2023-12-23 17:43:37 +0700

    combine file

--- index.js
+++ index.js
@@ -1,49 +1,16 @@
-// navbarHumberger
-const menuToggle = document.querySelector('.menu-toggle');
-const ul = document.querySelector('nav ul');
-const span1 = document.getElementsByTagName('span')[0];
-const span2 = document.getElementsByTagName('span')[1];
-const span3 = document.getElementsByTagName('span')[2];
-// const stuck = document.getElementsByTagName('body')[0];
-menuToggle.addEventListener('click' , function() {
-    ul.classList.toggle('slide');
-    span1.classList.toggle('slide');
-    span2.classList.toggle('slide');
-    span3.classList.toggle('slide');
-    menuToggle.classList.toggle('slide');
-    // stuck.classList.toggle('slide');
-    bodyDropdown.classList.remove('show')
-});
-//dropdown-contact
-const contackClick = document.querySelector('nav ul li p');
-const bodyDropdown = document.querySelector('.dropdown');
-contackClick.addEventListener('click' , function(e) {
-    bodyDropdown.classList.toggle('show');
-});
+// btn navbar
+const btn = document.querySelector("nav .btn");
+const span1 = document.querySelectorAll("nav .btn .menu-icon span")[0];
+const span2 = document.querySelectorAll("nav .btn .menu-icon span")[1];
+const containerNavbar = document.querySelector("nav .btn .container");
 
-//parallax
-const bg= document.querySelector('.bg');
-const bgImg= document.querySelector('.bgimg');
-// const nav = document.querySelector('nav');
-window.addEventListener('scroll' , function() {
-    let value = window.scrollY;
-    bgImg.style.top = value*.2 + 'px';
-    bg.style.top = value*0.1 + 'px';
-    // nav.style.top = value*.8 + 'px';
-    // parallaxLandingElement
-});
-
-// first open page - can use display a promo when user open this page
-// const banner = document.querySelector('.firstOpen');
-// window.addEventListener('click' , function() {
-//     banner.classList.add('delete');
-// });
-
-//alert(comingsoon)show
-const showAlert = document.querySelector('.alert')
-function show_alert() {
-    showAlert.classList.add('click');
-  };
-showAlert.addEventListener('click' , function() {
-    showAlert.classList.remove('click');
+btn.addEventListener('click' , function(e) {
+    // console.log(e.target);
+    // e.target.firstElementChild.classList.toggle('show');
+    // e.target.lastElementChild.classList.toggle('show');
+    // e.target.classList.toggle('show');
+    // e.target.classList.toggle('show');
+    span1.classList.toggle('show');
+    span2.classList.toggle('show');
+    containerNavbar.classList.toggle('show');
 });
