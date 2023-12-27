91865bbbd7743626d553539b36e8aadc88dcd475
Yofannnn <yofanapril17@gmail.com>
2023-12-28 06:24:29 +0700

    modify it a bit

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
-// btn navbar
-const btn = document.querySelector("nav .btn");
-const span1 = document.querySelectorAll("nav .btn .menu-icon span")[0];
-const span2 = document.querySelectorAll("nav .btn .menu-icon span")[1];
-const containerNavbar = document.querySelector("nav .btn .container");
-
-btn.addEventListener('click' , function(e) {
-    // console.log(e.target);
-    // e.target.firstElementChild.classList.toggle('show');
-    // e.target.lastElementChild.classList.toggle('show');
-    // e.target.classList.toggle('show');
-    // e.target.classList.toggle('show');
-    span1.classList.toggle('show');
-    span2.classList.toggle('show');
-    containerNavbar.classList.toggle('show');
+// click event btn menu
+const menuIcon = document.querySelector('nav .btn .menu-icon')
+menuIcon.addEventListener('click' , function(e) {
+    e.target.firstElementChild.classList.toggle('show');
+    e.target.lastElementChild.classList.toggle('show');
+    e.target.nextElementSibling.classList.toggle('show');
+    e.target.nextElementSibling.firstElementChild.classList.toggle('show');
+    e.target.nextElementSibling.lastElementChild.classList.toggle('show');
 });
